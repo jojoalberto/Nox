e04806b11d90446ed945d495553179f6bffd1144
JOSE-DESKTOP\jose <josegreogiro.alberto@gmail.com>
2025-04-17 17:51:29 +0100

    added UI image

--- Nox/Assets/Script/GamePlay/DialogueMessage.cs
+++ Nox/Assets/Script/GamePlay/DialogueMessage.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class DialogueMessage : MonoBehaviour
 {
     public List<string> dialogueMessage;
+    [SerializeField]
+    private Image image;
+
 
     public string GetDialogueMessage(int message)
     {
         return dialogueMessage[message];
+    }
+
+    public Image GetImage()
+    {
+        return image;
     }
 
 }
