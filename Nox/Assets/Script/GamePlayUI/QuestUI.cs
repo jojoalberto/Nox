363767f0f0aaebc1e438603e311b7d1eca5d4ebb
JOSE-DESKTOP\jose <josegreogiro.alberto@gmail.com>
2025-04-27 23:29:48 +0100

    added image for quest tracker

--- Nox/Assets/Script/GamePlayUI/QuestUI.cs
+++ Nox/Assets/Script/GamePlayUI/QuestUI.cs
@@ -2,22 +2,24 @@ using Photon.Pun;
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestUI : MonoBehaviour
 {
     public TextMeshProUGUI questText;
     private string tempText;
-
+    [SerializeField]
+    private Image image;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
 
@@ -25,6 +27,25 @@ public class QuestUI : MonoBehaviour
     {
         tempText = text;
         questText.text = tempText;
+    }
+
+    public void ShowImage(string itemID)
+    {
+        var data = ClueDatabase.GetClueByID(itemID);
+        if (data != null)
+        {
+            image.gameObject.SetActive(true);
+            image.sprite = data.image;
+        }
+        else
+        {
+            Debug.LogWarning($"ClueItemSO with ID '{itemID}' not found.");
+        }
+
+    }
+    public void HideImage()
+    {
+        image.gameObject.SetActive(false);
     }
 }
 
