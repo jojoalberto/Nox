c3877b320a054494fd76eb23c06d61eefcd3bfc5
JOSE-DESKTOP\jose <josegreogiro.alberto@gmail.com>
2025-04-30 23:36:55 +0100

    update interactable script

--- Nox/Assets/Script/GamePlay/InteractableObject.cs
+++ Nox/Assets/Script/GamePlay/InteractableObject.cs
@@ -172,7 +172,8 @@ public class InteractableObject : MonoBehaviourPunCallbacks
     public void ShowDialogue()
     {
         GameObject playerObj = PhotonNetwork.LocalPlayer.TagObject as GameObject;
-        protector = playerObj.GetComponent<Protector>();
+        protector = playerObj.GetComponentInChildren<Protector>();
+        Debug.Log("photon local player is: " + protector + protector.GetComponentInChildren<Protector>().isProtector);
         if (protector.isProtector == false)
         {
             if (dialogueManager != null)
