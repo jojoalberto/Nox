799c04d8273dcc5782fe4cdd4e5077d27e19e3d3
JOSE-DESKTOP\jose <josegreogiro.alberto@gmail.com>
2025-04-30 23:30:07 +0100

    getting component in children

--- Nox/Assets/Scrips/SpawnManager/PunPlayerSpawner.cs
+++ Nox/Assets/Scrips/SpawnManager/PunPlayerSpawner.cs
@@ -14,7 +14,7 @@ public class PunPlayerSpawner : MonoBehaviourPunCallbacks
         GameObject playerObj = PhotonNetwork.Instantiate(player.name, playerSpawnsLocation.transform.GetChild(PhotonNetwork.LocalPlayer.ActorNumber - 1).transform.position + new Vector3(-0.0875f, 2.979f, -8.35f), Quaternion.identity, 0);
         PhotonNetwork.LocalPlayer.TagObject = playerObj;
         GameObject tempPlayer = PhotonNetwork.LocalPlayer.TagObject as GameObject; ;
-        Debug.Log("photon local player is: " + tempPlayer + tempPlayer.GetComponent<Protector>().isProtector);
+        Debug.Log("photon local player is: " + tempPlayer + tempPlayer.GetComponentInChildren<Protector>().isProtector);
     }
 
     // Update is called once per frame
