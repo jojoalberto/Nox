a43c3bbad89364cfb69f7e869bab9071f42d2705
JOSE-DESKTOP\jose <josegreogiro.alberto@gmail.com>
2025-04-30 23:07:25 +0100

    test protector destroy

--- Nox/Assets/Script/PlayerScriptFolder/PlayerInventory.cs
+++ Nox/Assets/Script/PlayerScriptFolder/PlayerInventory.cs
@@ -10,6 +10,9 @@ public class PlayerInventory : MonoBehaviourPunCallbacks
     private void Start()
     {
         PhotonNetwork.LocalPlayer.TagObject = this.gameObject;
+        GameObject playerObj = PhotonNetwork.LocalPlayer.TagObject as GameObject;
+        Debug.Log("photon local player is: " + playerObj+ playerObj.GetComponent<Protector>().isProtector);
+
     }
 
     public bool IsHoldingVinyl() => heldVinyl != null;
